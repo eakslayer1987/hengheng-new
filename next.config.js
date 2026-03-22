d905b5e26166d9e944060a57d3b32a5c21d19f16
eakslayer <dellth.charulak.sur@gmail.com>
2026-03-22 13:48:46 +0700

    feat: proxy /hengheng/* to VPS

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['profile.line-scdn.net', 'lh3.googleusercontent.com'],
+    remotePatterns: [
+      { protocol: "https", hostname: "xn--72ca9ib1gc.xn--72cac8e8ec.com" },
+      { protocol: "https", hostname: "*.line-scdn.net" },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/hengheng/:path*',
+        destination: 'http://203.170.192.192/hengheng/:path*',
+      },
+    ]
   },
 }
-
 module.exports = nextConfig
